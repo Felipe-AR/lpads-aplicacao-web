b532aa9a597eb493e4bbd308fe25fba408d89ba2
Felipe-AR <fefeucn_cm@hotmail.com>
2021-11-24 23:46:39 -0300

    Mudança de funções assincronas para métodos assincronos

--- backend/src/controller/ClienteController.ts
+++ backend/src/controller/ClienteController.ts
@@ -2,37 +2,19 @@ import { Request, Response } from "express"
 import { getRepository } from "typeorm"
 import { Cliente, Endereco } from "../entity"
 
-export const getClientes = async (request: Request, response: Response) => {
-    const cliente = await getRepository(Cliente).find({ relations: ["endereco"] })
-    return response.json(cliente)
-}
-
-export const getCliente = async (request: Request, response: Response) => {
-    const { id } = request.params
-    const cliente = await getRepository(Cliente).findOne(id, { relations: ["endereco"] })
-    return response.json(cliente)
-}
-
-export const saveCliente = async (request: Request, response: Response) => {
-    const { endereco, ...body } = request.body
-
-    const enderecos = await getRepository(Endereco).find(endereco)
-    if (enderecos.length === 1) {
-        body.endereco = enderecos[0]
-    } else {
-        body.endereco = endereco
-        await getRepository(Endereco).save(endereco)
+class ClienteController {
+    async find(request: Request, response: Response) {
+        const { id } = request.params
+        const cliente = await getRepository(Cliente).findOne(id, { relations: ["endereco"] })
+        return response.json(cliente)
     }
+    async findAll(request: Request, response: Response) {
+        const cliente = await getRepository(Cliente).find({ relations: ["endereco"] })
+        return response.json(cliente)
+    }
+    async save(request: Request, response: Response) {
+        const { endereco, ...body } = request.body
 
-    const cliente = await getRepository(Cliente).save(request.body)
-    return response.json(cliente)
-}
-
-export const updateCliente = async (request: Request, response: Response) => {
-    const { id } = request.params
-    const { endereco, ...body } = request.body
-
-    if (endereco !== undefined) {
         const enderecos = await getRepository(Endereco).find(endereco)
         if (enderecos.length === 1) {
             body.endereco = enderecos[0]
@@ -40,20 +22,38 @@ export const updateCliente = async (request: Request, response: Response) => {
             body.endereco = endereco
             await getRepository(Endereco).save(endereco)
         }
-    }
 
-    const cliente = await getRepository(Cliente).update(id, body)
-    if (cliente.affected === 1) {
-        const clienteUpdated = await getRepository(Cliente).findOne(id, { relations: ["endereco"] })
-        return response.json(clienteUpdated)
+        const cliente = await getRepository(Cliente).save(request.body)
+        return response.json(cliente)
+    }
+    async update(request: Request, response: Response) {
+        const { id } = request.params
+        const { endereco, ...body } = request.body
+
+        if (endereco !== undefined) {
+            const enderecos = await getRepository(Endereco).find(endereco)
+            if (enderecos.length === 1) {
+                body.endereco = enderecos[0]
+            } else {
+                body.endereco = endereco
+                await getRepository(Endereco).save(endereco)
+            }
+        }
+
+        const cliente = await getRepository(Cliente).update(id, body)
+        if (cliente.affected === 1) {
+            const clienteUpdated = await getRepository(Cliente).findOne(id, { relations: ["endereco"] })
+            return response.json(clienteUpdated)
+        }
+    }
+    async delete(request: Request, response: Response) {
+        const { id } = request.params
+        const cliente = await getRepository(Cliente).findOne(id)
+        const clienteDeleted = await getRepository(Cliente).delete(id)
+        if (clienteDeleted.affected === 1) {
+            return response.json({ cliente, message: "O cliente foi removido com sucesso." })
+        }
     }
 }
 
-export const deleteCliente = async (request: Request, response: Response) => {
-    const { id } = request.params
-    const cliente = await getRepository(Cliente).findOne(id)
-    const clienteDeleted = await getRepository(Cliente).delete(id)
-    if (clienteDeleted.affected === 1) {
-        return response.json({ cliente, message: "O cliente foi removido com sucesso." })
-    }
-}
+export default new ClienteController()
--- backend/src/controller/OrcamentoController.ts
+++ backend/src/controller/OrcamentoController.ts
@@ -2,36 +2,36 @@ import { Request, Response } from "express"
 import { getRepository } from "typeorm"
 import { Orcamento } from "../entity"
 
-export const getOrcamentos = async (request: Request, response: Response) => {
-    const orcamentoGet = await getRepository(Orcamento).find({ relations: ["cliente", "servicos"] })
-    return response.json(orcamentoGet)
-}
-
-export const getOrcamento = async (request: Request, response: Response) => {
-    const { id } = request.params
-    const orcamento = await getRepository(Orcamento).findOne(id, { relations: ["cliente", "servicos"] })
-    return response.json(orcamento)
-}
-
-export const saveOrcamento = async (request: Request, response: Response) => {
-    const orcamento = await getRepository(Orcamento).save(request.body)
-    return response.json(orcamento)
-}
-
-export const updateOrcamento = async (request: Request, response: Response) => {
-    const { id } = request.params
-    const orcamento = await getRepository(Orcamento).update(id, request.body)
-    if (orcamento.affected === 1) {
-        const orcamentoUpdated = await getRepository(Orcamento).findOne(id, { relations: ["cliente"] })
-        return response.json(orcamentoUpdated)
+class OrcamentoController {
+    async find(request: Request, response: Response) {
+        const orcamentoGet = await getRepository(Orcamento).find({ relations: ["cliente", "servicos"] })
+        return response.json(orcamentoGet)
+    }
+    async findAll(request: Request, response: Response) {
+        const { id } = request.params
+        const orcamento = await getRepository(Orcamento).findOne(id, { relations: ["cliente", "servicos"] })
+        return response.json(orcamento)
+    }
+    async save(request: Request, response: Response) {
+        const orcamento = await getRepository(Orcamento).save(request.body)
+        return response.json(orcamento)
+    }
+    async update(request: Request, response: Response) {
+        const { id } = request.params
+        const orcamento = await getRepository(Orcamento).update(id, request.body)
+        if (orcamento.affected === 1) {
+            const orcamentoUpdated = await getRepository(Orcamento).findOne(id, { relations: ["cliente"] })
+            return response.json(orcamentoUpdated)
+        }
+    }
+    async delete(request: Request, response: Response) {
+        const { id } = request.params
+        const orcamento = await getRepository(Orcamento).findOne(id, { relations: ["cliente"] })
+        const orcamentoDeleted = await getRepository(Orcamento).delete(id)
+        if (orcamentoDeleted.affected === 1) {
+            return response.json({ orcamento, message: "O orcamento foi excluido com sucesso." })
+        }
     }
 }
 
-export const deleteOrcamento = async (request: Request, response: Response) => {
-    const { id } = request.params
-    const orcamento = await getRepository(Orcamento).findOne(id, { relations: ["cliente"] })
-    const orcamentoDeleted = await getRepository(Orcamento).delete(id)
-    if (orcamentoDeleted.affected === 1) {
-        return response.json({ orcamento, message: "O orcamento foi excluido com sucesso." })
-    }
-}
+export default new OrcamentoController()
